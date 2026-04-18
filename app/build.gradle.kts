3f02638dfd6975943617f1c81c1b0ee0d0c6deba
ST10345327 <ST10345327@RCCONNECT.EDU.ZA>
2026-04-18 02:08:06 +0200

    [ci] configure Safe

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
